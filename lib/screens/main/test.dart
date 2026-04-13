bf237b192266cad147676ffb21b55170bb974dd8
Hoa1012 <hoaphan101205@gmail.com>
2026-04-13 23:19:58 +0700

    update profile

---
+++ lib/screens/main/test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import '../profile/profile_screen.dart'; 
+
+void main() {
+  runApp(const TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ProfileScreen(),
+    );
+  }
+}
