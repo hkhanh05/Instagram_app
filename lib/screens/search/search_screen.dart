934370a68bf5dfd3526ede6378cb0b30fd6e92bc
babykit2005 <tkiet25052005@gmail.com>
2026-04-14 09:13:50 +0700

    Update search_screen.dart

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -6,16 +6,13 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-<<<<<<< HEAD
       body: Center(child: Text('Search Screen')),
     );
   }
 }
-=======
       body: Center(
         child: Text('Search Screen'),
       ),
     );
   }
 }
->>>>>>> 6327d897ed263bb7a2045b5003bf28aaa5c808f3
